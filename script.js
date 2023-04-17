cc7e838986e54bb232c1c6d6c369a8e0f3b5a2c8
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2023-04-17 16:29:47 -0300

    Le pongo un sonido al hacer hover sobre los proyectos

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const panels = document.querySelectorAll(".panel");
 
+const audio = new Audio(
+  "https://css-tricks.com/examples/SoundOnHover/audio/beep.mp3"
+);
+audio.volume = 0.5;
+
+panels.forEach((panel) => {
+  panel.addEventListener("mouseenter", () => {
+    if (panel.classList.contains("active")) return;
+    audio.play();
+    audio.currentTime = 0;
+  });
+});
+
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     const isActive = panel.classList.contains("active");
