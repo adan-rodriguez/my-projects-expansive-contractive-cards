1942f9f734fb32c2320d26a158049a231fc19f08
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2023-04-17 22:06:30 -0300

    Aplico algunos cambios

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const panels = document.querySelectorAll(".panel");
+const links = document.querySelectorAll(".panel a");
 
 const audio = new Audio(
   "https://css-tricks.com/examples/SoundOnHover/audio/beep.mp3"
 );
-audio.volume = 0.5;
+audio.volume = 0.1;
 
 panels.forEach((panel) => {
   panel.addEventListener("mouseenter", () => {
@@ -26,3 +27,9 @@ function removeActiveClass() {
     panel.classList.remove("active");
   });
 }
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    if (!link.parentElement.classList.contains("active")) e.preventDefault();
+  });
+});
